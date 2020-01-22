2dbcd4d70f8de04bbe71f1adfde434fbc6d5cc8c
Aymeric Dufresne <ADF@samsic.fr>
2020-01-22 17:18:15 +0100

    new template

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -15,7 +15,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /**
- * @Route("/user")
+ * @Route("/admin/user")
  */
 class UserController extends AbstractController
 {
